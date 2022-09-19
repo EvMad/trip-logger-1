868e6811d45eaf46eb253316ce391256c0a39c74
GitPit <rifflifter@gmail.com>
2022-09-19 14:55:38 -0400

    started apiRoutes imports etc

---
+++ server/routes/apiRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+import Trip from '../models/trips';
+
+
+// router.get("/api/trips", (req, res) => {
+//     Trip.aggregate([])
+// })
