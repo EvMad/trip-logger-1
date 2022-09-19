80cff8be2e5f91a94165879b045db407d6266bdf
GitPit <rifflifter@gmail.com>
2022-09-19 15:28:23 -0400

    wrote scripts for start/watch/seed

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,2 +1,24 @@
 import mongoose from 'mongoose';
 import Trip from '../models/trips';
+
+mongoose.connect('mongodb://localhost/trip-logger', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const tripSeed = [
+    {
+        date: '9/1/2022',
+        country: [
+          {
+            countryName: 'Italy',
+            region: 'Tuscany',
+            city: 'Florence',
+          },
+        ],
+    },
+
+]
+
+// Trip.deleteMany({}).then
