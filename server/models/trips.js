0cd219f1e8527087ae9c0a00a6b9b83b69d57430
GitPit <rifflifter@gmail.com>
2022-09-19 15:00:58 -0400

    added countryName, date, region, city to Schema

--- server/models/trips.js
+++ server/models/trips.js
@@ -2,7 +2,36 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const tripSchema = new Schema ({});
+const tripSchema = new Schema ({
+
+    date: {
+        type: Date,
+        default: () => new Date()
+    },
+
+    country: [
+        {
+            countryName: {
+                type: String,
+                trim: true,
+                required: "Enter country name"
+            },
+
+            region: {
+                type: String,
+                trim: true,
+                required: "Enter region for this date"
+            },
+
+            city: {
+                type: String,
+                trim: true,
+                required: "Enter municipality for this date"
+            }
+        }
+    ]
+
+});
 
 const trip = mongoose.model("Trip", tripSchema);
 
