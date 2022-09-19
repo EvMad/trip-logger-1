3495276618cecd8e1c39b282fc79206a8e6629cf
GitPit <rifflifter@gmail.com>
2022-09-19 14:27:31 -0400

    const app = express();

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ import morgan from 'morgan';
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
