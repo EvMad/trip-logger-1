834d26f7b6ebf6324df5de0ccdbb0b3d923c590e
GitPit <rifflifter@gmail.com>
2022-09-19 15:12:42 -0400

    wrote post route

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,7 +2,22 @@ import express from 'express';
 const router = express.Router();
 import Trip from '../models/trips';
 
+// get
+router.get("/api/trips", (req, res) => {
+    Trip([])
+    .catch(err => {
+        res.json(err);
+    });
+});
 
-// router.get("/api/trips", (req, res) => {
-//     Trip.aggregate([])
-// })
+// post
+
+router.post("/api/trips", ({ body }, res) => {
+    Trip.create(body)
+    .then(newTrip => {
+        res.json(newTrip);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
