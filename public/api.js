76a2816890ab1b527b5bc2ab65b2f0f32dad9733
GitPit <rifflifter@gmail.com>
2022-09-20 13:06:39 -0400

    getPreviousTrips(); async await addTrip();

--- public/api.js
+++ public/api.js
@@ -1,1 +1,32 @@
-const API = {};
+const API = {
+
+    async getPreviousTrips() {
+        let res;
+        try {
+            res = await fetch("api/trips");
+        } catch (err) {
+            console.log(err);
+        }
+
+        const json = await res.json();
+
+        // return json[json.length];
+
+        return json;
+
+    }, addTrip(data) {
+        const id = location.search.split("=")[1];
+
+        const res = await fetch("api/trips/" + id, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        });
+
+        const json = await res.json();
+
+        return json;
+
+    },
+
+};
