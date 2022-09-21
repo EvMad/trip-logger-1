e4aac7fa318c28949571e07d9f07b850c1037fbb
GitPit <rifflifter@gmail.com>
2022-09-21 16:21:17 -0400

    async function initTrips();

--- public/index.js
+++ public/index.js
@@ -7,7 +7,23 @@ const regionInput = document.querySelector("#region");
 const cityInput = document.querySelector("#city");
 
 
+
+
 // async function to recall trip data on init 
+
+async function initTrips() {
+    let trip;
+
+    if (location.search.split("=")[1] === undefined) {
+        trip = await API.createTrip()
+        console.log(trip)
+    }
+    if (trip) {
+        location.search = "?id" + trip._id;
+    }
+}
+
+initTrips();
 
 // handle change function to move to new field on click
 
