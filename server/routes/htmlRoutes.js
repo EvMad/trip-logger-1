fc26579c6c40d60fb01d1157b83e90da77363eb1
GitPit <rifflifter@gmail.com>
2022-09-23 15:48:17 -0400

    router.get for "/" public/index.html ??

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -4,5 +4,11 @@ const router = require('express').Router();
 const path = require('path');
 
 //router.get for each page view
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "././public/index.html"));
+});
+
+
+
 
 module.exports = router;
